31298e9e63d46ed42f4453198cc948cb3c848132
Akash Kumar <akashyadav112314@gmail.com>
2025-10-26 02:37:07 +0530

    add: dependency of hilt for Dependency Injection and Room's version upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp.plugin)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -45,7 +46,10 @@ android {
 dependencies {
     implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
+    ksp(libs.hilt.compiler)
     implementation(libs.androidx.room.ktx)
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
